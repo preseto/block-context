f58025f1f11c0b77a325684f327eacf65bdf3456
Kaspars Dambis <hi@kaspars.net>
2019-03-01 14:11:06 +0200

    Add own block

--- php/BlockContextPlugin.php
+++ php/BlockContextPlugin.php
@@ -7,6 +7,7 @@ namespace Preseto\BlockContext;
  */
 class BlockContextPlugin {
 
+	const BLOCK_SCRIPT_ID = 'preseto-block-context-block';
 
 	/**
 	 * Plugin interface.
@@ -30,6 +31,7 @@ class BlockContextPlugin {
 	 * @return void
 	 */
 	public function init() {
+		add_action( 'init', [ $this, 'register_blocks' ] );
 		add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_editor_assets' ] );
 	}
 
@@ -50,6 +52,38 @@ class BlockContextPlugin {
 				'lodash',
 			],
 			$this->plugin->asset_version()
+		);
+	}
+
+	public function register_blocks() {
+		wp_register_script(
+			self::BLOCK_SCRIPT_ID,
+			$this->plugin->asset_url( 'js/dist/block.js' ),
+			[
+				'wp-compose',
+				'wp-element',
+				'wp-editor',
+				'wp-components',
+				'lodash',
+			],
+			$this->plugin->asset_version()
+		);
+
+		wp_register_style(
+			self::BLOCK_SCRIPT_ID,
+			$this->plugin->asset_url( 'js/src/styles/block-context-block.css' ),
+			[
+				'wp-edit-blocks',
+			],
+			$this->plugin->asset_version()
+		);
+
+		register_block_type(
+			'preseto/block-context-block',
+			[
+				//'editor_script' => self::BLOCK_SCRIPT_ID,
+				'editor_style' => self::BLOCK_SCRIPT_ID,
+			]
 		);
 	}
 
