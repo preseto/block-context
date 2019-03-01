6470557411e056d30c8c977c67b42a3d13454a22
Kaspars Dambis <hi@kaspars.net>
2019-03-02 00:08:15 +0200

    Better naming

--- js/src/components/contexts/context-rule.js
+++ js/src/components/contexts/context-rule.js
@@ -3,16 +3,15 @@ const { SelectControl } = wp.components;
 
 function ContextRule( { value, onChange } ) {
 	const contextOptions = [
-		{ id: '', caption: __( 'Always show (default)', 'block-context' ) },
-		{ id: 'show', caption: __( 'Show block when', 'block-context' ) },
-		{ id: 'hide', caption: __( 'Hide block when', 'block-context' ) },
-		{ id: 'hidden', caption: __( 'Always hide', 'block-context' ) },
+		{ id: '', caption: __( 'Show always (default)', 'block-context' ) },
+		{ id: 'show', caption: __( 'Show block on selected', 'block-context' ) },
+		{ id: 'hide', caption: __( 'Hide block on selected', 'block-context' ) },
+		{ id: 'hidden', caption: __( 'Hide always', 'block-context' ) },
 	];
 
 	return (
 		<SelectControl
 			value={ value }
-			label={ __( 'Block Context', 'block-context' ) }
 			onChange={ onChange }
 			options={ contextOptions.map( ( option ) => ( {
 				label: option.caption,
