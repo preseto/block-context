7372765da841caefc30c702f8cd77fe54ce3307d
Kaspars Dambis <hi@kaspars.net>
2019-03-01 14:21:10 +0200

    Explain it all

--- js/src/editor.js
+++ js/src/editor.js
@@ -5,18 +5,21 @@ import blockContextAttributes from './block-context-attributes';
 const { registerBlockType } = wp.blocks;
 const { addFilter } = wp.hooks;
 
+// Add context attributes to all blocks.
 addFilter(
 	'blocks.registerBlockType',
 	'preseto/block-context/block-attributes',
 	blockContextAttributes
 );
 
+// Add context controls to all blocks.
 addFilter(
 	'editor.BlockEdit',
 	'preseto/block-context/block-controls',
 	BlockContextControls
 );
 
+// Register our context wrapper block.
 registerBlockType(
 	'preseto/block-context-block',
 	BlockContextBlock
