4418004feee9ee8fa7750148fba247d47b9b0e76
Kaspars Dambis <hi@kaspars.net>
2019-03-03 10:45:57 +0200

    Remove the block for now

--- js/src/editor.js
+++ js/src/editor.js
@@ -1,8 +1,6 @@
 import BlockContextControls from './components/block-context-controls';
-import BlockContextBlock from './block-context-block';
 import blockContextAttributes from './block-context-attributes';
 
-const { registerBlockType } = wp.blocks;
 const { addFilter } = wp.hooks;
 
 // Add context attributes to all blocks.
@@ -17,10 +15,4 @@ addFilter(
 	'editor.BlockEdit',
 	'preseto/block-context/block-controls',
 	BlockContextControls
-);
-
-// Register our context wrapper block.
-registerBlockType(
-	'preseto/block-context-block',
-	BlockContextBlock
 );
--- php/BlockContextPlugin.php
+++ php/BlockContextPlugin.php
@@ -7,10 +7,6 @@ namespace Preseto\BlockContext;
  */
 class BlockContextPlugin {
 
-	const BLOCK_CONTEXT_BLOCK_ID = 'preseto/block-context-block';
-
-	const BLOCK_CONTEXT_SCRIPT_ID = 'preseto-block-context-block';
-
 	/**
 	 * Plugin interface.
 	 *
@@ -43,9 +39,28 @@ class BlockContextPlugin {
 	public function init() {
 		$this->contexts->add( new Contexts\UserLoggedIn() );
 
-		add_action( 'init', [ $this, 'register_blocks' ] );
 		add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_editor_assets' ] );
 		add_filter( 'render_block', [ $this, 'maybe_hide_block' ], 5, 2 );
+	}
+
+	/**
+	 * Load our block assets.
+	 *
+	 * @return void
+	 */
+	public function enqueue_editor_assets() {
+		wp_enqueue_script(
+			'preseto-block-context-editor-js',
+			$this->plugin->asset_url( 'js/dist/editor.js' ),
+			[
+				'wp-compose',
+				'wp-element',
+				'wp-editor',
+				'wp-components',
+				'lodash',
+			],
+			$this->plugin->asset_version()
+		);
 	}
 
 	/**
@@ -103,58 +118,6 @@ class BlockContextPlugin {
 		}
 
 		return false;
-	}
-
-	/**
-	 * Load our block assets.
-	 *
-	 * @return void
-	 */
-	public function enqueue_editor_assets() {
-		wp_enqueue_script(
-			'preseto-block-context-editor-js',
-			$this->plugin->asset_url( 'js/dist/editor.js' ),
-			[
-				'wp-compose',
-				'wp-element',
-				'wp-editor',
-				'wp-components',
-				'lodash',
-			],
-			$this->plugin->asset_version()
-		);
-	}
-
-	public function register_blocks() {
-		wp_register_script(
-			self::BLOCK_CONTEXT_SCRIPT_ID,
-			$this->plugin->asset_url( 'js/dist/block.js' ),
-			[
-				'wp-compose',
-				'wp-element',
-				'wp-editor',
-				'wp-components',
-				'lodash',
-			],
-			$this->plugin->asset_version()
-		);
-
-		wp_register_style(
-			self::BLOCK_CONTEXT_SCRIPT_ID,
-			$this->plugin->asset_url( 'js/src/styles/block-context-block.css' ),
-			[
-				'wp-edit-blocks',
-			],
-			$this->plugin->asset_version()
-		);
-
-		register_block_type(
-			self::BLOCK_CONTEXT_BLOCK_ID,
-			[
-				//'editor_script' => self::BLOCK_SCRIPT_ID,
-				'editor_style' => self::BLOCK_CONTEXT_SCRIPT_ID,
-			]
-		);
 	}
 
 }
