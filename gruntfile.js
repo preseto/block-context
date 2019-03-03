6736498b7938303997438b0b68972a7e5b70a92e
Kaspars Dambis <hi@kaspars.net>
2019-03-03 11:40:14 +0200

    Add build tools

---
+++ gruntfile.js
@@ -0,0 +1,164 @@
+/* jshint es3: false, esversion: 5, node: true */
+
+var handlebars = require( 'handlebars' );
+var ignoreParse = require( 'parse-gitignore' );
+
+module.exports = function( grunt ) {
+
+	// Load all Grunt plugins.
+	require( 'load-grunt-tasks' )( grunt );
+
+	// TODO: Move to own Grunt plugin.
+	grunt.registerTask( 'readmeMdToTxt', 'Build the readme', function() {
+
+		var formatReadme = function( content ) {
+			var replaceRules = {
+				'#': '=== $1 ===',
+				'##': '== $1 ==',
+				'#{3,}': '= $1 =',
+			};
+
+			// Replace Markdown headings with WP.org style headings
+			Object.keys( replaceRules ).forEach( function( pattern ) {
+				var patternRegExp = [ '^', pattern, '\\s(.+)$' ].join( '' );
+
+				content = content.replace(
+					new RegExp( patternRegExp, 'gm' ),
+					replaceRules[ pattern ]
+				);
+			} );
+
+			return content;
+		};
+
+		var replaceVars = function( content, vars ) {
+			var template = handlebars.compile( content );
+
+			return template( vars );
+		};
+
+		var getPluginVersion = function( pluginFile ) {
+			var pluginSource = grunt.file.read( pluginFile );
+			var pattern = new RegExp( 'Version:\\s*(.+)$', 'mi' );
+			var match = pluginSource.match( pattern );
+
+			if ( match.length ) {
+				return match[1];
+			}
+
+			return null;
+		};
+
+		var path = require('path');
+		var pkgConfig = grunt.config.get( 'pkg' );
+
+		var options = this.options( {
+			src: 'readme.md',
+			dest: 'readme.txt',
+		} );
+
+		var srcFile = grunt.file.read( options.src );
+		var destDir = path.dirname( options.dest );
+
+		// Extract the version from the main plugin file.
+		if ( 'undefined' === typeof pkgConfig.version ) {
+			var pluginVersion = getPluginVersion( 'block-context.php' );
+
+			if ( ! pluginVersion ) {
+				grunt.warn( 'Failed to parse the plugin version in the plugin file.' );
+			}
+
+			pkgConfig.version = pluginVersion;
+		}
+
+		// Replace all variables.
+		var readmeTxt = replaceVars( srcFile, pkgConfig );
+
+		// Ensure we have the destination directory.
+		if ( destDir ) {
+			grunt.file.mkdir( destDir );
+		}
+
+		// Write the readme.txt.
+		grunt.file.write( options.dest, formatReadme( readmeTxt ) );
+
+	});
+
+	// Get a list of all the files and directories to exclude from the distribution.
+	var distignore = ignoreParse( '.distignore', {
+		invert: true,
+	} );
+
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		dist_dir: 'dist/block-context',
+
+		clean: {
+			build: [
+				'<%= dist_dir %>',
+			],
+			js: [
+				'js/dist',
+			],
+		},
+
+		readmeMdToTxt: {
+			options: {
+				src: 'readme.txt.md',
+				dest: '<%= dist_dir %>/readme.txt',
+			},
+		},
+
+		copy: {
+			dist: {
+				src: [ '**' ].concat( distignore ),
+				dest: '<%= dist_dir %>',
+				expand: true,
+			}
+		},
+
+		wp_deploy: {
+			options: {
+				plugin_slug: 'block-context',
+				svn_user: 'kasparsd',
+				build_dir: '<%= dist_dir %>',
+				assets_dir: 'assets/wporg',
+			},
+			all: {
+				options: {
+					deploy_tag: true,
+					deploy_trunk: true,
+				}
+			},
+			trunk: {
+				options: {
+					deploy_tag: false,
+				}
+			}
+		},
+	} );
+
+	grunt.registerTask(
+		'release', [
+			'clean:build',
+			'copy',
+			'readmeMdToTxt',
+		]
+	);
+
+	grunt.registerTask(
+		'deploy', [
+			'release',
+			'wp_deploy:all',
+		]
+	);
+
+	grunt.registerTask(
+		'deploy-trunk', [
+			'release',
+			'wp_deploy:trunk',
+		]
+	);
+
+};
