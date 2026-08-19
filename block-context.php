2c695e47a5769c02207259c94c93fda4d2506225
Kaspars Dambis <hi@kaspars.net>
2026-08-19 17:24:36 +0300

    tag as beta

--- block-context.php
+++ block-context.php
@@ -3,7 +3,7 @@
  * Plugin Name: Block Context
  * Plugin URI: https://blockcontext.com
  * Description: Hide Gutenberg editor blocks in context.
- * Version: 0.2.0
+ * Version: 0.2.0-beta
  * Author: Kaspars Dambis
  * Author URI: https://kaspars.net
  * Requires PHP: 7.4
